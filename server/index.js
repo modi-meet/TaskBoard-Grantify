878d68a13a7f9b3b9753b88d87e2e7bac6c983c3
Modi Meet <meetmodi8080@gmail.com>
2025-12-11 11:05:05 +0530

    feat: fix typo in task controller naming

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const taskRoutes = require('./routes/taskRoutes')
 
 dotenv.config()
+connectDB()
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -10,9 +13,7 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json({ message: 'API is running' })
-})
+app.use('/api/tasks', taskRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
